84fa404643817fcc64c5e3529651210133208985
nick <nick219@yandex.ru>
2024-01-29 18:12:40 +0300

    [5.2.x] upd: bump version in controller

--- src/Rsse.Service/Controllers/TestController.cs
+++ src/Rsse.Service/Controllers/TestController.cs
@@ -32,7 +32,7 @@ public class TestController : Controller
     [HttpGet("get/version")]
     public ActionResult GetVersion()
     {
-        return Ok("v5.1.2: K3S and DV SSL ready");
+        return Ok("v5.2.0: .NET8 + React 18");
     }
 
     // TODO последующие ручки переписать либо удалить:
