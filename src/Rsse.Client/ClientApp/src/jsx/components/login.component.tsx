8b26d4a8881e7750b25425a0bc6fe62b1fe91209
nick <nick219@yandex.ru>
2024-02-01 00:51:03 +0300

    [5.2.x] functional components: login

--- src/Rsse.Client/ClientApp/src/jsx/components/login.component.tsx
+++ src/Rsse.Client/ClientApp/src/jsx/components/login.component.tsx
@@ -2,13 +2,8 @@
 import { Loader } from "./loader.tsx";
 import { getPageNumber } from "../dto/handler.catalog.tsx";
 import { createRoot } from "react-dom/client";
-import { ISimpleProps } from "../contracts/i.simple.props.tsx";
-import { Component } from "react";
+import { Component, useState } from "react";
 import { IMountedComponent } from "../contracts/i.mounted.tsx";
-
-interface IState {
-    style: CSSStyleDeclaration|string;
-}
 
 declare global {
     interface Window { textId: number, temp: Component & IMountedComponent, url: string, pageNumber: number|undefined }
@@ -19,8 +14,68 @@ export class LoginBoxHandler {
     static loginMessageElement = document.querySelector("#loginMessage") ?? document.createElement('loginMessage');
     static loginMessageRoot = createRoot(this.loginMessageElement);
 
+    static Invisible = () => {
+        if (!LoginBoxHandler.login) return;
+        (document.getElementById("loginMessage") as HTMLElement).style.display = "none";
+    }
+
+    static Visible = (component: Component & IMountedComponent, url: string) => {
+        window.temp = component;
+        window.url = url;
+        const state: Readonly<object> = component.state;
+        if (Object.prototype.hasOwnProperty.call(state, 'data')) {
+            const data = 'data' as keyof typeof state;
+            window.pageNumber = getPageNumber(state[data]);
+        }
+
+        (document.getElementById("loginMessage") as HTMLElement).style.display = "block";
+        (document.getElementById("login") as HTMLElement).style.display = "block";
+        LoginBoxHandler.login = true;
+        LoginBoxHandler.loginMessageRoot.render(
+            <h1>
+                LOGIN PLEASE
+            </h1>
+        );
+    }
+}
+
+export const LoginComponent = () => {
+    const [style, setStyle] = useState("submitStyle");
+
+    let loginElement = document.getElementById("login") as HTMLElement ?? document.createElement('login');
+    loginElement.style.display = "block";
+
+    const submit = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        let email = "test@email";
+        let password = "password";
+        let emailElement = document.getElementById('email') as HTMLInputElement;
+        let passwordElement = document.getElementById('password') as HTMLInputElement;
+        if (emailElement) email = emailElement.value;
+        if (passwordElement) password = passwordElement.value;
+
+        let query = "?email=" + String(email) + "&password=" + String(password);
+        let callback = (response: Response) => response.ok ? loginOk() : loginErr();
+        Loader.fireAndForgetWithQuery(Loader.loginUrl, query, callback, null);
+    }
+
+    const loginErr = () => {
+        document.cookie = 'rsse_auth = false';
+        console.log("Login error");
+    }
+
+    const loginOk = () => {
+        document.cookie = 'rsse_auth = true';
+        console.log("Login ok.");
+        setStyle("submitStyleGreen");
+        continueLoading();
+        setTimeout(() => {
+            (document.getElementById("login") as HTMLElement).style.display = "none";
+        }, 1500);
+    }
+
     // восстанавливаем данные (но не последнее действие), не полученные из-за ошибки авторизации:
-    static ContinueLoading() {
+    const continueLoading = () => {
         let component = window.temp;
 
         if (component) {
@@ -38,101 +93,23 @@ export class LoginBoxHandler {
             }
         }
 
-        this.Invisible();
+        LoginBoxHandler.Invisible();
     }
 
-    static Invisible() {
-        if (!this.login) return;
-        (document.getElementById("loginMessage")as HTMLElement).style.display = "none";
-    }
-
-    // hideMenu ?
-    static Visible(component: Component & IMountedComponent, url: string) {
-        window.temp = component;
-        window.url = url;
-        const state: Readonly<object> = component.state;
-        if (Object.prototype.hasOwnProperty.call(state, 'data')){
-            const data = 'data' as keyof typeof state;
-            window.pageNumber = getPageNumber(state[data]);
-        }
-
-        (document.getElementById("loginMessage")as HTMLElement).style.display = "block";
-        (document.getElementById("login")as HTMLElement).style.display = "block";
-        this.login = true;
-        LoginBoxHandler.loginMessageRoot.render(
-            <h1>
-                LOGIN PLEASE
-            </h1>
-        );
-    }
-}
-
-export class LoginComponent extends React.Component<ISimpleProps, IState> {
-
-    public state: IState = {
-        style: "submitStyle"
-    }
-
-    constructor(props: ISimpleProps) {
-        super(props);
-        this.submit = this.submit.bind(this);
-
-        let loginElement = document.getElementById("login") as HTMLElement ?? document.createElement('login');
-        loginElement.style.display = "block";
-    }
-
-    submit(e: React.SyntheticEvent) {
-        e.preventDefault();
-        let email = "test@email";
-        let password = "password";
-        let emailElement = document.getElementById('email') as HTMLInputElement;
-        let passwordElement = document.getElementById('password') as HTMLInputElement;
-        if (emailElement) email = emailElement.value;
-        if (passwordElement) password = passwordElement.value;
-
-        let query = "?email=" + String(email) + "&password=" + String(password);
-        let callback = (response: Response) => response.ok ? this.loginOk() : this.loginErr();
-
-        Loader.fireAndForgetWithQuery(Loader.loginUrl, query, callback, null);
-    }
-
-    loginErr = () => {
-        document.cookie = 'rsse_auth = false';
-        console.log("Login error");
-    }
-
-    loginOk = () => {
-        document.cookie = 'rsse_auth = true';
-        console.log("Login ok.");
-        this.setState({ style: "submitStyleGreen" });
-        LoginBoxHandler.ContinueLoading();
-        setTimeout(() => {
-            (document.getElementById("login")as HTMLElement).style.display = "none";
-        }, 1500);
-    }
-
-    componentWillUnmount() {
-        // отменяй подписки и асинхронную загрузку
-    }
-
-    render() {
-        return (
-            <div>
-                <div id={String(this.state.style)}>
-                    <input type="checkbox" id="loginButton" className="regular-checkbox" onClick={this.submit} />
-                    <label htmlFor="loginButton">Войти</label>
-                </div>
-                {/*<div id={this.state.style}>*/}
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <span>
-                    <input type="text" id="email" name="email" autoComplete={ "on" } />
-                </span>
-                {/*<div id={this.state.style}>*/}
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <span>
-                    <input type="text" id="password" name="password" autoComplete={ "on" } />
-                </span>
+    return (
+        <div>
+            <div id={style}>
+                <input type="checkbox" id="loginButton" className="regular-checkbox" onClick={submit}/>
+                <label htmlFor="loginButton">Войти</label>
             </div>
-        );
-    }
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <span>
+                    <input type="text" id="email" name="email" autoComplete={"on"}/>
+                </span>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <span>
+                    <input type="text" id="password" name="password" autoComplete={"on"}/>
+                </span>
+        </div>
+    );
 }
