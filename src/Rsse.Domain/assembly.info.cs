5afd945150eae442c7971b55302f573477884a77
Nikolay Medvedev <nick219@yandex.ru>
2025-06-28 04:09:32 +0300

    upd: чистка assembly.info

--- src/Rsse.Domain/assembly.info.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Rsse.Service")]
-[assembly: InternalsVisibleTo("Rsse.Tests")]
-[assembly: InternalsVisibleTo("Rsse.Integration.Tests")]
